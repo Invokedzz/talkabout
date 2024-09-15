09fcd080cfc7bd378fbaa59350a75a860bea8ee2
Samuel Nóbrega <162361529+Invokedzz@users.noreply.github.com>
2024-09-15 16:46:05 -0300

    exporting database

--- src/database.ts
+++ src/database.ts
@@ -2,4 +2,23 @@ import mysql from "mysql2/promise";
 
 import dotenv from "dotenv";
 
-dotenv.config({ path: "./.env" });
+dotenv.config({
+    path: __dirname + '/file.env' });
+
+const userkey = process.env.SQL_USER;
+
+const passwordkey = process.env.SQL_PASSWORD;
+
+export const createPool = mysql.createPool({
+
+    host: 'localhost',
+    user: userkey,
+    password: passwordkey,
+    database: 'talkabout',
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10,
+    idleTimeout: 60000,
+    queueLimit: 0
+
+});
