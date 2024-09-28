45b18b6fd6a2c3a7b9b3c924a40acdd27540e279
Samuel Nóbrega <162361529+Invokedzz@users.noreply.github.com>
2024-09-28 10:00:37 -0300

    unit tests :(

--- src/tests/unit.test.ts
+++ src/tests/unit.test.ts
@@ -1,4 +1,4 @@
-import { loginGETmiddleware, registerGETmiddleware, homemiddleware, createtopicGETmiddleware, createcommentsGETmiddleware } from "../middlewares";
+import { loginGETmiddleware, registerGETmiddleware, homemiddleware, createtopicGETmiddleware, createcommentsGETmiddleware, deletecommentPOSTmiddleware, deletetopicmiddleware, viewtopicsmiddleware, createtopicPOSTmiddleware } from "../middlewares";
 
 import { Request, Response } from "express";
 
@@ -105,6 +105,202 @@ describe("Rendering the comments page", (): void => {
         createcommentsGETmiddleware(Request as Request, Response as Response);
 
         expect(Response.render).toHaveBeenCalledWith("comments", {topicid: "1"});
+
+    });
+
+});
+
+describe("Test for delete comment", (): void => {
+
+    let Request: Partial <Request>;
+
+    let Response: Partial <Response>;
+
+    const mockQuery = jest.fn();
+
+    beforeEach((): void => {
+
+        Request = {
+
+            body: {
+
+                id: "1",
+
+            },
+
+        };
+
+        Response = {
+
+            redirect: jest.fn(),
+
+        };
+
+    });
+
+    afterEach((): void => {
+
+        jest.clearAllMocks();
+
+    });
+
+    (createPool.query as jest.Mock) = mockQuery;
+
+    it ("Should delete the comment successfully", async (): Promise <void> => {
+        
+        await deletecommentPOSTmiddleware(Request as Request, Response as Response);
+
+        expect(Response.redirect).toHaveBeenCalledWith("/home");
+
+    });
+
+});
+
+describe("Testing the delete topic method", (): void => {
+
+    let Request: Partial <Request>;
+
+    let Response: Partial <Response>;
+
+    const mockQuery = jest.fn();
+
+    beforeEach((): void => {
+
+        Request = {
+
+            params: {
+
+                id: "10",
+
+            },
+
+        };
+
+        Response = {
+
+            redirect: jest.fn(),
+
+        };
+
+        (createPool.query as jest.Mock) = mockQuery;
+
+    });
+
+    afterEach((): void => {
+
+        jest.clearAllMocks();
+
+    });
+
+    it ("Should delete something successfully", async (): Promise <void> => {
+
+        mockQuery.mockResolvedValue(null);
+
+        await deletetopicmiddleware(Request as Request, Response as Response);
+
+        expect(mockQuery).toHaveBeenCalledWith('DELETE FROM topics WHERE id = ?', ["10"]);
+
+        expect(Response.redirect).toHaveBeenCalledWith("/viewtopics");
+
+    });
+
+});
+
+describe("Test for creating topics", (): void => {
+
+    let Request: Partial <Request>;
+
+    let Response: Partial <Response>;
+
+    const mockQuery = jest.fn();
+
+    beforeEach((): void => {
+
+        Request = {};
+
+        Response = {
+
+            render: jest.fn(),
+
+        };
+
+        (createPool.query as jest.Mock) = mockQuery;
+
+    });
+
+    afterEach((): void => {
+
+        jest.clearAllMocks();
+
+    });
+
+    it ("Should view a topic", async (): Promise <void> => {
+
+        const mockTests = [{title: "testing", theme: "testing", text: "testing"}];
+
+        mockQuery.mockResolvedValueOnce([mockTests]);
+
+        await viewtopicsmiddleware(Request as Request, Response as Response);
+
+        expect(mockQuery).toHaveBeenCalledWith('SELECT * FROM topics');
+
+        expect(Response.render).toHaveBeenCalledWith("viewtopics", { topics: mockTests });
+
+    });
+
+});
+
+describe("Testing topic post", (): void => {
+
+    let Request: Partial <Request>;
+
+    let Response: Partial <Response>
+
+    const mockQuery = jest.fn();
+
+    beforeEach((): void => {
+
+        Request = {
+
+            body: {
+            
+                title: "testing",
+
+                theme: "testing",
+
+                text: "testing",
+            
+            },
+
+        };
+
+        Response = {
+
+            render: jest.fn(),
+
+        };
+
+        (createPool.query as jest.Mock) = mockQuery;
+
+    });
+
+    afterEach((): void => {
+
+        jest.clearAllMocks();
+
+    });
+
+    it ("Should return the values successfully", async (): Promise <void> => {
+
+        const testMocks = [{title: "testing", theme: "testing", text: "testing"}];
+
+        mockQuery.mockResolvedValue(testMocks);
+
+        await createtopicPOSTmiddleware(Request as Request, Response as Response);
+
+        expect(mockQuery).toHaveBeenCalledWith('INSERT INTO topics (title, theme, text) VALUES (?, ?, ?)', ["testing", "testing", "testing"]);
+
+        expect(Response.render).toHaveBeenCalledWith("receivetopics", { title: "testing" });
 
     });
 
